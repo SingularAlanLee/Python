59fc8b75ea5b64c5caa98f85457bfe6053a25469
Alan Lee <91273523+SingularAlanLee@users.noreply.github.com>
2021-10-10 20:56:33 -0700

    Add files via upload

---
+++ madLibs.py
@@ -0,0 +1,18 @@
+def madLibs():
+  print("Good evening I'm Norm Macdonald and here's the fake news.")
+
+  movie1 = input("Enter a movie title: ")
+  movie2 = input("Enter another movie title: ")
+  movie3 = input("Enter one more movie title: ")
+  print(f"Monday is Oscar night, and three films, {movie1}, {movie2}, and {movie3} are locked in a tight race in the category: Best picture there's not a chance I will ever see.")
+
+  celebrity1 = input("Enter a celebrity's name: ")
+  celebrity2 = input("Enter another celebrity's name: ")
+  print(f"{celebrity1}'s longtime partner, {celebrity2}, has written a new book called 'You Can Make it Happen: A Nine-Step Plan For Success.' Step No. 1: become {celebrity1}'s partner. Then the other eight are just 'hang around.'")
+
+  celebrity1 = input("Enter the name of a celebrity: ")
+  celebrity2 = input("Enter the name of another celebrity: ")
+  print(f"Real estate mogul {celebrity1} announced this week that after 3½ years of marriage, they are seeking a divorce from wife, {celebrity2}. According to {celebrity1}, {celebrity2} violated part of their marriage agreement when they decided to turn 30.")
+
+  animal = input("Enter the name of an animal: ")
+  print(f"In London, British scientists created a {animal} embryo without a head, a breakthrough that could lead to the production of headless human clones to provide organs and tissue for transplant, as well as horrific nightmares for the rest of my life.")
